78c4de52fc1d49d57f4e288c06bad48b7e5a9ff9
Leandro <leandro.arias.sm@gmail.com>
2024-08-23 19:12:40 -0400

    proyecto base

---
+++ src/scripts.js
@@ -0,0 +1,55 @@
+function encriptar () {
+    
+    let textoIngresado = document.getElementById('textarea').value
+    document.getElementById('btn-copy').classList.remove('btn-click-copy')
+    document.getElementById('img-out').classList.add('img-2-show')
+
+       const reemplazos = {
+        'e' : 'enter',
+        'i' : 'imes',
+        'a' : 'ai',
+        'o' : 'ober',
+        'u' : 'ufat'}
+
+        let textoEncriptado = textoIngresado.replace(/[aeiou]/g, function(match) {
+            return reemplazos[match];
+        });
+        
+        // Actualiza el contenido de un párrafo con el texto encriptado
+        document.querySelector('p').textContent = textoEncriptado;
+    }
+
+function desencriptar () {
+    let textoIngresado = document.getElementById('textarea').value
+    document.getElementById('btn-copy').classList.remove('btn-click-copy')
+    document.getElementById('img-out').classList.add('img-2-show')
+    const reemplazos = {
+          'enter':'e',
+          'imes' :'i',
+          'ai'   :'a',
+          'ober' :'o',
+          'ufat' :'u'}
+
+          let textoDesencriptado = textoIngresado;
+
+
+          for (let key in reemplazos) {
+            let regExp = new RegExp(key, 'g');
+            textoDesencriptado = textoDesencriptado.replace(regExp, reemplazos[key]);
+        }
+        
+        // Actualiza el contenido de un párrafo con el texto encriptado
+        document.querySelector('p').textContent = textoDesencriptado;
+
+}
+
+ 
+function copiarTexto(idTxt){
+   let blq = document.getElementById(idTxt);
+   let txt = blq.innerHTML;
+   let btn = event.target;
+   let btnLbl = btn.innerText;
+   navigator.clipboard.writeText(txt);
+   btn.innerHTML = "Copiado";
+   setTimeout(()=>btn.innerText = btnLbl, 1200);
+}
